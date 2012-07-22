6c49ac3889848c79d1e60a0dae6971f1c4722f96
Pavel Pozdniak <pozdpl@gmail.com>
2012-07-22 18:44:15 +1100

    Add sphinx config as dependancy for the audio recognizer

--- src/main/java/ru/urbancamper/audiobookmarker/audio/AudioFileRecognizer.java
+++ src/main/java/ru/urbancamper/audiobookmarker/audio/AudioFileRecognizer.java
@@ -3,7 +3,16 @@
  */
 package ru.urbancamper.audiobookmarker.audio;
 
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import ru.urbancamper.audiobookmarker.text.RecognizedTextOfSingleAudiofile;
 
 /**
@@ -11,16 +20,65 @@ import ru.urbancamper.audiobookmarker.text.RecognizedTextOfSingleAudiofile;
  * @author pozpl
  */
 public class AudioFileRecognizer {
-    public RecognizedTextOfSingleAudiofile recognize(String filePath){
-        File file = new File(filePath);
-        return this.recognize(file);
+
+    private ConfigurationManager sphinxConfigurationManager;
+
+    public AudioFileRecognizer(ConfigurationManager sphinxConfigManager){
+        this.sphinxConfigurationManager = sphinxConfigManager;
     }
-    public RecognizedTextOfSingleAudiofile recognize(File file){
-        //@TODO Implement this
+
+    public RecognizedTextOfSingleAudiofile recognize(URL fileURL){
+        
+
         return null;
     }
+
+
+    public RecognizedTextOfSingleAudiofile recognize(String filePath){
+        URL fileURL = null;
+        try {
+            fileURL = new URL(filePath);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(AudioFileRecognizer.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        return this.recognize(fileURL);
+    }
+
 
     private String getFileHash(File file){
         return "";
     }
+
+
+
+//     public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
+//
+//        ConfigurationManager cm = new ConfigurationManager("config/sphinx-custom.xml");
+//        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+//
+//        TextAlignerGrammar grammar = (TextAlignerGrammar) cm.lookup("textAlignGrammar");
+//
+//        recognizer.addResultListener(grammar);
+//
+//        /* allocate the resource necessary for the recognizer */
+//        recognizer.allocate();
+//
+//        // configure the audio input for the recognizer
+//        AudioFileDataSource dataSource = (AudioFileDataSource) cm.lookup("audioFileDataSource");
+//
+//        dataSource.setAudioFile(new URL("file:media/1-test-got.wav"), null);
+//
+//        Result result;
+//        String resultTextAggregated = "";
+//        while ((result = recognizer.recognize()) != null) {
+//
+//            String resultText = result.getTimedBestResult(false, true);
+//            System.out.println(resultText);
+//            resultTextAggregated += resultText;
+//        }
+//        System.out.println(resultTextAggregated + "\n");
+//    }
+
 }
