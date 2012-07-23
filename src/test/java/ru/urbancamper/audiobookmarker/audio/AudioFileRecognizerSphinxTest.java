8c139231e140402c3674546f500e3b6e6cd7dc24
Pavel Pozdniak <pozdpl@gmail.com>
2012-07-24 08:48:38 +1100

    Move file into a test branch.

--- src/test/java/ru/urbancamper/audiobookmarker/audio/AudioFileRecognizerSphinxTest.java
+++ src/test/java/ru/urbancamper/audiobookmarker/audio/AudioFileRecognizerSphinxTest.java
@@ -4,8 +4,14 @@
  */
 package ru.urbancamper.audiobookmarker.audio;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.urbancamper.audiobookmarker.audio.AudioFileRecognizerSphinx;
 import ru.urbancamper.audiobookmarker.context.BeansAnnotationsForTests;
 
 /**
@@ -14,8 +20,22 @@ import ru.urbancamper.audiobookmarker.context.BeansAnnotationsForTests;
  */
 public class AudioFileRecognizerSphinxTest {
 
-    public void main() {
+    /**
+     *
+     * @param arg
+     */
+    public static void main(String arg[]) {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         ApplicationContext ctxt = new AnnotationConfigApplicationContext(BeansAnnotationsForTests.class);
-        AudioFileRecognizerSphinx ercognizer = ctxt.getBean(AudioFileRecognizerSphinx.class);
+        AudioFileRecognizerSphinx recognizer = ctxt.getBean(AudioFileRecognizerSphinx.class);
+
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileInputStream("src/main/resources/test.properties"));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(AudioFileRecognizerSphinx.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        recognizer.recognize(null, null)
     }
 }
