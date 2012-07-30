5daab77630294c77dbd34cc0c829f5ee43976c84
Pavel Pozdniak <pozdpl@gmail.com>
2012-07-31 07:32:14 +1100

    Add function to get file name from absolute path.

--- src/main/java/ru/urbancamper/audiobookmarker/AudioBookMarkerUtil.java
+++ src/main/java/ru/urbancamper/audiobookmarker/AudioBookMarkerUtil.java
@@ -4,6 +4,8 @@
  */
 package ru.urbancamper.audiobookmarker;
 
+import java.io.File;
+import java.util.HashMap;
 import ru.urbancamper.audiobookmarker.audio.AudioFileRecognizerInterface;
 import ru.urbancamper.audiobookmarker.text.BookText;
 import ru.urbancamper.audiobookmarker.text.RecognizedTextOfSingleAudiofile;
@@ -18,6 +20,8 @@ public class AudioBookMarkerUtil {
 
     private AudioFileRecognizerInterface audioRecognizer;
 
+    private HashMap<String, String> audioFilesIdentificatorMap;
+
     public AudioBookMarkerUtil(BookText bookText, AudioFileRecognizerInterface audioRecognizer){
         this.bookTextAggregator = bookText;
         this.audioRecognizer = audioRecognizer;
@@ -26,6 +30,7 @@ public class AudioBookMarkerUtil {
     public String makeMarkers(String[] audioBookFilesPaths, String fullText){
         this.bookTextAggregator.setFullText(fullText);
         for(String audioFilePath: audioBookFilesPaths){
+
             RecognizedTextOfSingleAudiofile recognizedFile = this.audioRecognizer.recognize(audioFilePath, audioFilePath);
             this.bookTextAggregator.registerRecognizedTextPiece(recognizedFile);
         }
@@ -33,5 +38,10 @@ public class AudioBookMarkerUtil {
         return markedText;
     }
 
+    private String getAudioFileName(String audioFilePath){
+        String fileName;
+        fileName = new File(audioFilePath).getName();
+        return fileName;
+    }
 
 }
