638dc785eceb0895f41530767a041784084e4ffb
Pavel Pozdniak <pozdpl@gmail.com>
2012-08-01 07:28:21 +1100

    Stub for testing.

---
+++ src/test/java/ru/urbancamper/audiobookmarker/AudioBookMarkerUtilTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.urbancamper.audiobookmarker;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author pozpl
+ */
+public class AudioBookMarkerUtilTest extends TestCase {
+
+    public AudioBookMarkerUtilTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testMakeMarkers() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
