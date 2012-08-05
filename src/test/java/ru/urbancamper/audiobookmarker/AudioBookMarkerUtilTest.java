b80f2afa14ccf0ccb2d1b442c811a0e43ee9b1d4
Pavel Pozdniak <pozdpl@gmail.com>
2012-08-06 07:39:58 +1100

    Test for file name -> unic identifier mapping.

--- src/test/java/ru/urbancamper/audiobookmarker/AudioBookMarkerUtilTest.java
+++ src/test/java/ru/urbancamper/audiobookmarker/AudioBookMarkerUtilTest.java
@@ -4,6 +4,7 @@
  */
 package ru.urbancamper.audiobookmarker;
 
+import java.util.HashMap;
 import junit.framework.TestCase;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -51,7 +52,12 @@ public class AudioBookMarkerUtilTest extends TestCase {
         MarkedDocument markedDocument = util.makeMarkers(filePaths, this.BOOK_TEXT);
         String markedText =  markedDocument.getMarkedText();
 
-        System.out.println("|" + markedText + "|");
         assertEquals("there is <1:2.1/>some <1:3.0/>kind of magnificent pease <1:4.5/>of literature <1:6.1/>text <1:9.0/>here", markedText);
+
+        HashMap<String, String> filesNamesToUidMap = markedDocument.getFileNamesToUidsMap();
+        HashMap<String, String> filesNamesToUidMapExpected = new HashMap<String, String>();
+        filesNamesToUidMapExpected.put("path", String.valueOf(0));
+        assertEquals(filesNamesToUidMapExpected, filesNamesToUidMap);
+
     }
 }
