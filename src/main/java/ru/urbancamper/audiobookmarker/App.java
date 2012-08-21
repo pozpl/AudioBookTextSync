cb49ea33071ada7b9c7d7f1f49f5343ab24bf3b3
Pavel Pozdniak <pozdpl@gmail.com>
2012-08-22 07:35:30 +1100

    Start to implement functions to get file list and full text from files;

--- src/main/java/ru/urbancamper/audiobookmarker/App.java
+++ src/main/java/ru/urbancamper/audiobookmarker/App.java
@@ -1,18 +1,29 @@
 package ru.urbancamper.audiobookmarker;
 
+import java.io.File;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.urbancamper.audiobookmarker.context.BeansAnnotations;
 import ru.urbancamper.audiobookmarker.text.LanguageModelBasedTextTokenizer;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App{
+    private String[] getAudioFilesPaths(String directoryPath){
+        File directory = new File(directoryPath);
+        String[] filePathsList = directory.list();
+        return filePathsList;
+    }
+
+    private String getBookFullText(String txtFilePath){
+        //TODO IMplement this
+        return null;
+    }
+
+    public static void main( String[] args ){
 
         ApplicationContext ctxt = new AnnotationConfigApplicationContext(BeansAnnotations.class);
         String text = "test, token model.";
