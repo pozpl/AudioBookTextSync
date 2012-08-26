21e5015277b5c137b6186822c94722fa2482f454
Pavel Pozdniak <pozdpl@gmail.com>
2012-08-27 07:04:49 +1100

    Simple function to get file full text/

--- src/main/java/ru/urbancamper/audiobookmarker/App.java
+++ src/main/java/ru/urbancamper/audiobookmarker/App.java
@@ -1,17 +1,29 @@
 package ru.urbancamper.audiobookmarker;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.urbancamper.audiobookmarker.context.BeansAnnotations;
 import ru.urbancamper.audiobookmarker.text.LanguageModelBasedTextTokenizer;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 /**
  * Hello world!
  *
  */
 public class App{
+
+    /**
+     *  Logging facility
+     */
+    protected final Log logger = LogFactory.getLog(getClass());
+
+
     private String[] getAudioFilesPaths(String directoryPath){
         File directory = new File(directoryPath);
         String[] filePathsList = directory.list();
@@ -19,8 +31,21 @@ public class App{
     }
 
     private String getBookFullText(String txtFilePath){
-        //TODO IMplement this
-        return null;
+        StringBuilder strBuffer = new StringBuilder();
+        int BLOC_SIZE = 512;
+        char[] b = new char[BLOC_SIZE];
+        Reader fileReader;
+        try {
+            fileReader = new FileReader(txtFilePath);
+            int n;
+            while((n = fileReader.read(b))>0){
+                strBuffer.append(b, 0, n);
+            }
+        } catch (IOException ex) {
+            this.logger.error("Excsption during file read " + txtFilePath + ": " + ex);
+        }
+        String retText = strBuffer.toString();
+        return retText;
     }
 
     public static void main( String[] args ){
