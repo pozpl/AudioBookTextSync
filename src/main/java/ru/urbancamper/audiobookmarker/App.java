ca9483a4512214766d9bd05c7c27737cd7ade567
Pavel Pozdniak <pozdpl@gmail.com>
2012-08-30 07:26:03 +1100

    Simple cli interface to main application.

--- src/main/java/ru/urbancamper/audiobookmarker/App.java
+++ src/main/java/ru/urbancamper/audiobookmarker/App.java
@@ -1,34 +1,57 @@
 package ru.urbancamper.audiobookmarker;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.urbancamper.audiobookmarker.context.BeansAnnotations;
-
+import ru.urbancamper.audiobookmarker.document.MarkedDocument;
 
 /**
  * Hello world!
  *
  */
-public class App{
+public class App {
 
     /**
-     *  Logging facility
+     * Logging facility
      */
     protected final Log logger = LogFactory.getLog(getClass());
 
-    private void getCliOptions(String[] arg){
-        Options options = new Options();
-        options.addOption("a", "audio", true, "Path to a directory contaning audiobook files");
+    private CommandLine getCliOptions(String[] args) {
+        CommandLine cmd = null;
+        try {
+            Options options = new Options();
+            options.addOption("a", "audio", true, "Path to a directory contaning audiobook files");
+            options.addOption("b", "book", true, "A txt File that contains book text.");
 
+            CommandLineParser parser = new PosixParser();
+            cmd = parser.parse(options, args);
+        } catch (ParseException ex) {
+            logger.error("Error while parsing cli arguments" + ex);
+        }
+        return cmd;
     }
 
-    public static void main( String[] args ){
-            ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeansAnnotations.class);
+
+
+    public static void main(String[] args) {
+        App app = new App();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeansAnnotations.class);
         AudioBookMarkerUtil audioBookMarkerUtil;
         audioBookMarkerUtil = applicationContext.getBean(AudioBookMarkerUtil.class);
 
+        CommandLine cmd = app.getCliOptions(args);
+        String audioBookDirPath = cmd.getOptionValue("a");
+        String bookFilePath = cmd.getOptionValue("b");
+
+        MarkedDocument markedDoc = audioBookMarkerUtil.makeMarkers(audioBookDirPath, bookFilePath);
     }
 }
