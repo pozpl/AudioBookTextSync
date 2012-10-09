fd3b1a90777f464e30a28df0cf20f34e7a09c545
Pavel Pozdniak <pozdpl@gmail.com>
2012-10-10 07:57:11 +1100

    Slightly fixed tests for a recursion version

--- src/test/java/ru/urbancamper/audiobookmarker/text/LongestSubsequenceFinderTest.java
+++ src/test/java/ru/urbancamper/audiobookmarker/text/LongestSubsequenceFinderTest.java
@@ -74,20 +74,22 @@ public class LongestSubsequenceFinderTest extends TestCase {
         assertEquals(expResult, result);
     }
 
-   
+
     /**
      * Test of longestSubsequenceLengthWithDistanceCorrection method, of class LongestSubsequenceFinder.
      */
     public void testLongestSubsequenceLengthWithDistanceCorrection() {
         System.out.println("longestSubsequenceLengthWithDistanceCorrection");
-        Integer[] fullArray = null;
-        Integer[] subArray = null;
         LongestSubsequenceFinder instance = new LongestSubsequenceFinder();
-        TreeMap expResult = null;
-        TreeMap result = instance.longestSubsequenceLengthWithDistanceCorrection(fullArray, subArray);
+        TreeMap<Integer, Integer> expResult = new TreeMap<Integer, Integer>();
+        expResult.put(Integer.valueOf(0), Integer.valueOf(1));
+        expResult.put(Integer.valueOf(2), Integer.valueOf(2));
+        expResult.put(Integer.valueOf(4), Integer.valueOf(3));
+        expResult.put(Integer.valueOf(5), Integer.valueOf(4));
+
+        TreeMap<Integer, Integer> result = instance.longestSubsequenceLengthWithDistanceCorrection(this.fullText, this.partialText);
+
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
 }
