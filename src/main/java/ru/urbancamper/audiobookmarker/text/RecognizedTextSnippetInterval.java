483e1d135b96b87affffffbe45b52708b7e5e8db
Pavel Pozdniak <pozdpl@gmail.com>
2012-11-17 22:44:57 +1100

    Annotation for a new function.

--- src/main/java/ru/urbancamper/audiobookmarker/text/RecognizedTextSnippetInterval.java
+++ src/main/java/ru/urbancamper/audiobookmarker/text/RecognizedTextSnippetInterval.java
@@ -10,4 +10,17 @@ package ru.urbancamper.audiobookmarker.text;
  */
 public class RecognizedTextSnippetInterval {
 
+    /**
+     * This function calculate two bounds in a full text array in which  a recognized
+     * snippet are probably reside.
+     * @param fullText full text in integers array form
+     * @param subText sub text in integers array form
+     * @return Integer[] bounds indexes of first word and last word to plase
+     * recognized text to.
+     */
+    public Integer[] calculateFullTextBoundsForRecognizedSnippet(
+            Integer[] fullText, Integer[] subText){
+        return null;
+    }
+
 }
