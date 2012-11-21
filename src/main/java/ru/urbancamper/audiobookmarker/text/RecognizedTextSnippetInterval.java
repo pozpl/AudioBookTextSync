489c504e79290d1ab308a02127dd8dcc44fbe8e9
Pavel Pozdniak <pozdpl@gmail.com>
2012-11-22 07:45:46 +1100

    Delete unused functions for sets uperations

--- src/main/java/ru/urbancamper/audiobookmarker/text/RecognizedTextSnippetInterval.java
+++ src/main/java/ru/urbancamper/audiobookmarker/text/RecognizedTextSnippetInterval.java
@@ -176,35 +176,10 @@ public class RecognizedTextSnippetInterval {
         return aggregatedSumm;
     }
 
-    public <T> Set<T> union(Set<T> setA, Set<T> setB) {
+    private <T> Set<T> union(Set<T> setA, Set<T> setB) {
         Set<T> tmp = new TreeSet<T>(setA);
         tmp.addAll(setB);
         return tmp;
-    }
-
-    public static <T> Set<T> intersection(Set<T> setA, Set<T> setB) {
-        Set<T> tmp = new TreeSet<T>();
-        for (T x : setA) {
-            if (setB.contains(x)) {
-                tmp.add(x);
-            }
-        }
-        return tmp;
-    }
-
-    public static <T> Set<T> difference(Set<T> setA, Set<T> setB) {
-        Set<T> tmp = new TreeSet<T>(setA);
-        tmp.removeAll(setB);
-        return tmp;
-    }
-
-    public <T> Set<T> symDifference(Set<T> setA, Set<T> setB) {
-        Set<T> tmpA;
-        Set<T> tmpB;
-
-        tmpA = union(setA, setB);
-        tmpB = intersection(setA, setB);
-        return difference(tmpA, tmpB);
     }
 
     /**
