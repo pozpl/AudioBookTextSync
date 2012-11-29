32bf6289dc2d182d3fc0b140a4be1a9eceb5c256
Pavel Pozdniak <pozdpl@gmail.com>
2012-11-30 07:41:25 +1100

    New test sequences in book text tests

--- src/test/java/ru/urbancamper/audiobookmarker/text/BookTextTest.java
+++ src/test/java/ru/urbancamper/audiobookmarker/text/BookTextTest.java
@@ -23,7 +23,7 @@ public class BookTextTest extends TestCase {
 
     private static String[] recognizedTextStubs = {
         "word1(0.1, 1.4) word2(1.5, 2.0) word3(2.1, 2.4) word4(2.6, 3.0) ",
-        "word4(3.1, 3.4) word3(4.1, 4.4) word2(5.1, 5.4) word1(6.1, 6.4) ",
+        "word5(3.1, 3.4) word3(4.1, 4.4) word3(5.1, 5.4) word1(6.1, 6.4) ",
 //        "word1(7.1, 7.4) word2(8.1, 8.4) word3(9.1, 9.4) word4(10.1, 10.4) ",
 //        "word3(11.1, 11.4) word2(12.1, 12.4) word3(13.1, 13.4) word4(14.1, 14.4) ",
 //        "word1(15.1, 15.4) word2(16.1, 16.4) word3(17.1, 17.4) word4(18.1, 18.4) ",
@@ -32,10 +32,10 @@ public class BookTextTest extends TestCase {
     private static String[]  audioFilesHashes = {"1", "2", "3", "4", "5"};
 
     private static String bookFullText = "word1 word2 word3 noword word4"
-            + " word1 word1 word1  word4 word3 noword noword nword word2 word1";
+            + " word1 word1 word1  word5 word3 noword word1 nword word2 word1";
     private static String bookFullTextWithMarks =
-            "<1:0.1/>word1 <1:1.5/>word2 <1:2.1/>word3 noword <1:2.6/>word4"
-            + " word1 word1 word1 <2:3.1/>word4 <2:4.1/>word3 noword noword nword <2:5.1/>word2 <2:6.1/>word1";
+            "<1:0.1/>word1 <1:1.5/>word2 <1:2.1/>word3 noword word4"
+            + " word1 word1 word1 <2:3.1/>word5 <2:4.1/>word3 noword <2:6.1>word1 nword word2 word1";
 
     public BookTextTest(String testName) {
         super(testName);
@@ -141,11 +141,11 @@ public class BookTextTest extends TestCase {
         }
 
         Method method =
-          RecognizedTextSnippetInterval.class.getDeclaredMethod("getLongestSubsequenceMappingFromRecognizedTexts",
-                new Class[]{});
+          RecognizedTextSnippetInterval.class.getDeclaredMethod("getLongestSubsequenceMappingFromRecognizedTexts");
+//                new Class[]{});
         method.setAccessible(true);
         ArrayList<TreeMap<Integer, Integer>> output;
-        output = (ArrayList<TreeMap<Integer, Integer>>) method.invoke(instance, new Object[]{});
+        output = (ArrayList<TreeMap<Integer, Integer>>) method.invoke(instance);//, new Object[]{});
 
     }
 }
