b3b2470c5da607f173afc296723582b05a9ac8e3
Pavel Pozdniak <pozdpl@gmail.com>
2012-12-06 08:09:24 +1100

    Start ot implement bitap algorithm for sub text finding in full text array.

---
+++ src/main/java/ru/urbancamper/audiobookmarker/text/BitapSubtextFinding.java
@@ -0,0 +1,33 @@
+/*
+ * This class is supposed to find snippet of text in full text pattern
+ * by means of Bitap algorithm
+ */
+package ru.urbancamper.audiobookmarker.text;
+
+/**
+ *
+ * @author pozpl
+ */
+public class BitapSubtextFinding {
+
+
+    /**
+     * Shift bytes array on n positions to the right
+     * @param bytes
+     * @param rightShifts
+     */
+    private static void shiftBitsRight(byte[] bytes, final int rightShifts) {
+        assert rightShifts >= 1 && rightShifts <= 7;
+
+        final int leftShifts = 8 - rightShifts;
+
+        byte previousByte = bytes[0]; // keep the byte before modification
+        bytes[0] = (byte) (((bytes[0] & 0xff) >> rightShifts) | ((bytes[bytes.length - 1] & 0xff) << leftShifts));
+        for (int i = 1; i < bytes.length; i++) {
+            byte tmp = bytes[i];
+            bytes[i] = (byte) (((bytes[i] & 0xff) >> rightShifts) | ((previousByte & 0xff) << leftShifts));
+            previousByte = tmp;
+        }
+    }
+
+}
