3848c903ab7328646e07bcb1ceaaa7db8ce7d7cc
Pavel Pozdniak <pozdpl@gmail.com>
2012-12-07 07:42:51 +1100

    Tests template  for bitap functions

---
+++ src/test/java/ru/urbancamper/audiobookmarker/text/BitapSubtextFindingTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.urbancamper.audiobookmarker.text;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author pozpl
+ */
+public class BitapSubtextFindingTest extends TestCase {
+
+    public BitapSubtextFindingTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testFillByteArrayFromWordsNumbersArray() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
