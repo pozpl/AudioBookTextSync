8488c82199caf62dfb0571ec030453bf3082918c
Pavel Pozdniak <pozdpl@gmail.com>
2013-05-23 08:04:03 +1100

    master Implemented bare text to document map with naive approach that don't take in to account format of a document that can contain words equal with bare text ones

--- src/main/java/ru/urbancamper/audiobookmarker/text/formats/BareTextToDocumentMap.java
+++ src/main/java/ru/urbancamper/audiobookmarker/text/formats/BareTextToDocumentMap.java
@@ -18,9 +18,21 @@ public class BareTextToDocumentMap {
      * so for each bara word we can say exact position in document
      * @param bareText - array of bare text words
      * @param document - string that contains representation of text
-     * @return
+     * @return  Tree map of bareText word index to first character of this word in document
      */
     public TreeMap<Integer, Integer> mapBareTextToDocument(String[] bareText, String document){
-        return new TreeMap<Integer, Integer>();
+        Integer documentOffset = 0;
+        Integer doclastCharacterIndex = document.length() - 1;
+        TreeMap<Integer, Integer> bareTextToDocumentMap = new TreeMap<Integer, Integer>();
+        for(Integer bareTextWordIndex = 0; bareTextWordIndex < bareText.length; bareTextWordIndex++){
+            String bareTextWord = bareText[bareTextWordIndex];
+            Integer bareWordIndexInDoc = document.indexOf(bareTextWord, documentOffset);
+            documentOffset = bareWordIndexInDoc + bareTextWord.length() - 1;
+            bareTextToDocumentMap.put(bareTextWordIndex, bareWordIndexInDoc);
+            if(documentOffset >= doclastCharacterIndex){
+                break;
+            }
+        }
+        return bareTextToDocumentMap;
     }
 }
